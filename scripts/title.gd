e2f084afb9dcc3c0db1bbd4a64062320cbd9c45f
VivanPrasad <vivan.prasad25@gmail.com>
2025-10-05 03:25:27 -0700

    asset update

--- scripts/title.gd
+++ scripts/title.gd
@@ -2,6 +2,7 @@ extends Control
 
 @onready var buttons: VBoxContainer = $Buttons
 const GAME = preload("uid://q4j5knjnfbxn")
+const TUTORIAL = preload("uid://doirtr655e1no")
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -9,12 +10,19 @@ func _ready() -> void:
 
 func connect_buttons() -> void:
 	for child in buttons.get_children():
+		child.mouse_entered.connect(_on_button_hovered.bind(child))
+		child.mouse_exited.connect(_on_button_exited.bind(child))
 		child.pressed.connect(_on_button_pressed.bind(child))
 
+func _on_button_hovered(button: TextureButton) -> void:
+	create_tween().tween_property(button,"modulate", Color(1.3, 1.3, 1.3, 1.0), 0.1)
+func _on_button_exited(button: TextureButton) -> void:
+	create_tween().tween_property(button,"modulate", Color.WHITE , 0.1)
+	
 func _on_button_pressed(button: TextureButton) -> void:
 	match button.name:
 		&"Play":
-			get_tree().change_scene_to_packed(GAME)
+			get_tree().change_scene_to_packed(TUTORIAL)
 		&"Quit":
 			get_tree().quit()
 # Called every frame. 'delta' is the elapsed time since the previous frame.
--- scripts/ui/tutorial.gd
+++ scripts/ui/tutorial.gd
@@ -1,16 +1,44 @@
 extends Control
 
-const DIALOGUE := [
-	"This castle is asleep, but my night has just begun.
-	After years and years, my rat army has been formed.
-	I must raid the treasuries of  five kindgoms, and you must help me!"
+const INTRO := [
+	"This castle is asleep, but my night has just begun...",
+	"After years and years, my rat army has been formed.",
+	"I must raid the treasuries of five kingdoms.",
+	"Bring them back to me with all the gold."
 ]
+var tween: Tween
+var line: int = -1
+@onready var text: Label = $TextureRect/Label
+@onready var label: Label =  $TextureRect/Label2
+
+const GAME = preload("uid://q4j5knjnfbxn")
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	line = 0
+	speak()
+	
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
+func speak() -> void:
+	if line > len(INTRO) - 1: 
+		line = -1
+		get_tree().change_scene_to_packed(GAME)
+	text.visible_ratio = 0.0
+	text.text = INTRO[line]
+	tween = create_tween()
+	tween.tween_property(text,^"visible_ratio",1.0,0.8)
+
 func _process(_delta: float) -> void:
-	pass
+	label.visible = bool(text.visible_ratio == 1.0)
+
+func _input(_event: InputEvent) -> void:
+	if Input.is_action_just_pressed("ui_accept") \
+			and line != -1:
+		if tween and tween.is_running():
+			text.visible_ratio = 1.0
+			tween.kill()
+		else:
+			line += 1
+			speak()
