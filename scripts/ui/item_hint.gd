81af3e6159eacaa7e675419ced22bae139815ee0
VivanPrasad <vivan.prasad25@gmail.com>
2025-10-05 07:27:47 -0700

    item placement and level 2 added

--- scripts/ui/item_hint.gd
+++ scripts/ui/item_hint.gd
@@ -23,8 +23,8 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
-	var mouse_pos = get_viewport().get_mouse_position()
-	set_position(mouse_pos.snapped(Vector2.ONE * 128 ))
+	var mouse_pos = get_viewport().get_mouse_position() + Vector2.ONE * 10
+	set_position(mouse_pos.snapped(Vector2.ONE * 128 * 0.3))
 	var level = (get_parent() as Game).get_level()
 
 	if Game.gold < Game.ITEM_COSTS[Game.item_selected]:
