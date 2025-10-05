40756f859335d2c60576c8a1c6ffdf4d04098b76
VivanPrasad <vivan.prasad25@gmail.com>
2025-10-05 06:23:44 -0700

    hinting update

--- scripts/game.gd
+++ scripts/game.gd
@@ -1,15 +1,18 @@
-extends Node2D
+class_name Game extends Node2D
 
 @onready var camera: Camera2D = $Camera2D
 @onready var background: = $BG/Background
 @onready var entities: Node2D = $Entities
-@onready var level: TileMapLayer = $Level
 @onready var button: Button = $HUD/MarginContainer/Button
 @onready var torch: PointLight2D = $Torch
 @onready var timer: Timer = $Timer
 @onready var info: Label = $HUD/MarginContainer/ColorRect/Label
+@onready var items: HBoxContainer = $HUD/MarginContainer/Items
+@onready var levels: Node2D = $Levels
 
-const CAMERA_SCROLL_STR := 0.005
+@onready var gold_label: Label = $HUD/MarginContainer/Label
+
+const CAMERA_SCROLL_STR := 0.0005
 
 const BASE_RAT_COUNT := 20
 var rat_count: int = BASE_RAT_COUNT
@@ -19,49 +22,52 @@ const MIN_ZOOM := 0.1
 
 const RAT = preload("uid://dsk2bhusk47gt")
 const GOLD = preload("uid://cyubd1tbfy40k")
+const ITEM_HINT = preload("uid://cdd7sitsj0bio")
 
 var gold: int = 10
+var level: int = 1
 func _ready() -> void:
 	#generate_level()
+	info.get_parent().hide()
+	connect_items()
 	randomize_tiles()
 
 func generate_level() -> void:
-	level.clear()
+	get_level().clear()
 	for x in range(-2,31):
-		level.set_cell(Vector2i(x, -2), 0, Vector2.ZERO)
-		level.set_cell(Vector2i(x, -1), 0, Vector2.ZERO)
-		level.set_cell(Vector2i(x, 17), 0, Vector2.ZERO)
-		level.set_cell(Vector2i(x, 18), 0, Vector2.ZERO)
+		get_level().set_cell(Vector2i(x, -2), 0, Vector2.ZERO)
+		get_level().set_cell(Vector2i(x, 17), 0, Vector2.ZERO)
+		get_level().set_cell(Vector2i(x, 18), 0, Vector2.ZERO)
 
 	# Vertical borders (left and right)
 	for y in range(-2,18):
-		level.set_cell(Vector2i(-2, y), 0, Vector2.ZERO)
-		level.set_cell(Vector2i(-1, y), 0, Vector2.ZERO)
-		level.set_cell(Vector2i(30, y), 0, Vector2.ZERO)
-		level.set_cell(Vector2i(31, y), 0, Vector2.ZERO)
+		get_level().set_cell(Vector2i(-2, y), 0, Vector2.ZERO)
+		get_level().set_cell(Vector2i(-1, y), 0, Vector2.ZERO)
+		get_level().set_cell(Vector2i(30, y), 0, Vector2.ZERO)
+		get_level().set_cell(Vector2i(31, y), 0, Vector2.ZERO)
 	for y in range(1,17):
 		for x in range(1, 29):
 			if randi() % 5 == 0:
-				level.set_cell(Vector2(x,y), 0, Vector2.ZERO)
+				get_level().set_cell(Vector2(x,y), 0, Vector2.ZERO)
 	
-	var cell = level.get_used_cells().pick_random()
+	var cell = get_level().get_used_cells().pick_random()
 	while cell.x < 1 or cell.x > 28 or cell.y < 1 or cell.y > 16:
-		cell = level.get_used_cells().pick_random()
-	level.set_cell(cell, 0, Vector2(1,1))
+		cell = get_level().get_used_cells().pick_random()
+	get_level().set_cell(cell, 0, Vector2(1,1))
 	var gold_bag = GOLD.instantiate()
-	gold_bag.position = level.to_global(cell)
+	gold_bag.position = get_level().to_global(cell)
 	add_child(gold_bag)
 	
 func randomize_tiles() -> void:
 	randomize()
-	var used_cells = level.get_used_cells()
+	var used_cells = get_level().get_used_cells()
 	for cell in used_cells:
 		var random_tile = randi() % 5
-		if level.get_cell_atlas_coords(cell).y == 0:
-			level.set_cell(cell, 0, Vector2(random_tile, 0))
-		elif level.get_cell_atlas_coords(cell) == Vector2i.ONE:
+		if get_level().get_cell_atlas_coords(cell).y == 0:
+			get_level().set_cell(cell, 0, Vector2(random_tile, 0))
+		elif get_level().get_cell_atlas_coords(cell) == Vector2i.ONE:
 			var gold_bag = GOLD.instantiate()
-			gold_bag.position = level.to_global(cell) * 128 + Vector2.ONE * 20
+			gold_bag.position = get_level().to_global(cell) * 128 + Vector2.ONE * 20
 			add_child(gold_bag)
 	used_cells = background.get_used_cells()
 	for cell in used_cells:
@@ -79,18 +85,22 @@ func spawn_rats() -> void:
 
 func _process(_delta: float) -> void:
 	var mouse_pos = get_viewport().get_mouse_position()
-	var screen_center = get_viewport().get_visible_rect().size / 2.0
-	var offset = screen_center + (mouse_pos * CAMERA_SCROLL_STR)
+	#var screen_center = get_viewport().get_visible_rect().size / 2.0
+	#var offset = screen_center + (mouse_pos * CAMERA_SCROLL_STR)
 	torch.global_position = mouse_pos + Vector2.ONE * 10
-	background.position = -(mouse_pos * CAMERA_SCROLL_STR) / 2.0
-	camera.offset = offset
+	#background.position = -(mouse_pos * CAMERA_SCROLL_STR) / 2.0
+	#camera.offset = offset
 	
-	update_hud()
+	if not timer.is_stopped():
+		update_hud()
 
 func update_hud() -> void:
-	info.text = "Time Left: %d\nGold: %d\n Rats Left: %d" % [ceil(timer.time_left), gold, entities.get_child_count()]
-	
-func _input(_event: InputEvent) -> void:
+	info.get_parent().show()
+	info.text = "Time Left: %d\nRats Left: %d" % [ceil(timer.time_left), entities.get_child_count()]
+	gold_label.text = "Gold %d" % gold
+
+
+func _input(event: InputEvent) -> void:
 	if Input.is_action_just_pressed(&"scroll_up"):
 		camera.zoom = clamp(camera.zoom + (Vector2.ONE * 0.1), Vector2.ONE * MIN_ZOOM, Vector2.ONE * MAX_ZOOM)
 	elif Input.is_action_just_pressed(&"scroll_down"):
@@ -102,9 +112,37 @@ func _input(_event: InputEvent) -> void:
 		else:
 			Engine.set_time_scale(1.0)
 	
+	if event is InputEventMouseButton:
+		if event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
+			if item_selected != Item.NONE:
+				print("Left mouse button pressed")
+			
+func connect_items() -> void:
+	for item in items.find_children("*","TextureButton"):
+		(item as TextureButton).pivot_offset = Vector2.ONE * 36
+		(item as TextureButton).mouse_entered.connect(
+			func(): create_tween().tween_property(item, "scale", Vector2.ONE * 1.1, 0.1)
+		)
+		(item as TextureButton).mouse_exited.connect(
+			func(): create_tween().tween_property(item, "scale", Vector2.ONE, 0.1)
+		)
+		(item as TextureButton).pressed.connect(
+			_on_item_selected.bind(item)
+		)
 
-
+enum Item {GARLIC, CHEESE, SPRING, NONE = -1}
+static var item_selected: Item = Item.NONE
+func _on_item_selected(item: TextureButton) -> void:
+	item_selected = item.get_parent().get_index() as Item
+	match item.name:
+		_:
+			var hint := ITEM_HINT.instantiate()
+			add_child(hint)
+	
+func get_level() -> TileMapLayer:
+	return levels.get_child(level - 1)
 func _on_raid_started() -> void:
 	button.hide()
 	timer.start()
 	spawn_rats()
+	
---
+++ scripts/ui/item_hint.gd
@@ -0,0 +1,38 @@
+class_name ItemHint extends Area2D
+
+@onready var sprite: Sprite2D = $Sprite2D
+
+const TILES = preload("uid://drrjqkhudsrej")
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	var atlas = AtlasTexture.new()
+	atlas.atlas = TILES.duplicate()
+	var rect = Rect2()
+	match Game.item_selected:
+		Game.Item.CHEESE:
+			rect = Rect2(0,128,128,128)
+		Game.Item.GARLIC:
+			rect = Rect2(384, 128, 128, 128)
+		Game.Item.SPRING:
+			rect = Rect2(512, 128, 128, 128)
+	atlas.set_region(rect)
+	sprite.set_texture(atlas)
+	modulate.a = 0.8
+	
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(_delta: float) -> void:
+	var mouse_pos = get_viewport().get_mouse_position()
+	set_position(mouse_pos.snapped(Vector2.ONE * 128 * 0.3))
+	var level = (get_parent() as Game).get_level()
+	if level.local_to_map(global_position) + Vector2i(0,1) in level.get_used_cells():
+		modulate = Color.LIME
+		modulate.a = 0.8
+	else:
+		modulate = Color.RED
+		modulate.a = 0.8
+		
+func _input(event) -> void:
+	if event is InputEventMouseButton:
+		if event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
+			queue_free()
