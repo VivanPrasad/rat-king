e5d119d493fbee2a93f86ff23a0dc1227c2bfb24
VivanPrasad <vivan.prasad25@gmail.com>
2025-10-04 21:31:12 -0700

    updated rat movement

--- scripts/entities/rat.gd
+++ scripts/entities/rat.gd
@@ -2,36 +2,46 @@ extends CharacterBody2D
 
 @onready var tree: AnimationTree = $AnimationTree
 @onready var sprite: Sprite2D = $Sprite2D
+@onready var ray: RayCast2D = $RayCast2D
+@onready var collision: CollisionShape2D = $CollisionShape2D
 
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
+const SPEED := 120.0
+const JUMP_VELOCITY := -350.0
+const RAY_LENGTH := 40.0
 
 enum State {RUN, JUMP, FALL}
 @export var state: State = State.RUN
 @export var direction: int = 0 # 1 = left, 0 = right
 
 func _physics_process(delta: float) -> void:
-	# Add the gravity.
 	var go := Vector2(-1 if direction else 1, 0)
+	ray.target_position.x = go.x * RAY_LENGTH
+	collision.position.x = go.x * 15.0
+	velocity.x = go.x * SPEED
+	match state:
+		State.RUN:
+			tree.get(&"parameters/playback").travel("Run")
+			tree.set(&"parameters/Run/blend_position", go.x)
+		State.JUMP:
+			tree.get(&"parameters/playback").travel("Jump")
+			tree.set(&"parameters/Jump/blend_position", go.x)
+		State.FALL:
+			tree.get(&"parameters/playback").travel("Fall")
+			tree.set(&"parameters/Fall/blend_position", go.x)
+			velocity.x = 0.5 * SPEED
+	
 	if not is_on_floor():
-		velocity += get_gravity() * delta
-		tree.get(&"parameters/playback").travel("Fall")
-		tree.set(&"parameters/Fall/blend_position", go.x)
-
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		tree.get(&"parameters/playback").travel("Jump")
-		tree.set(&"parameters/Jump/blend_position", go.x)
+		velocity.y += get_gravity().y * delta
+	
+	if ray.is_colliding() and is_on_floor():
 		velocity.y = JUMP_VELOCITY
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	if go and is_on_floor():
-		velocity.x = go.x * SPEED
-		tree.get(&"parameters/playback").travel("Run")
-		tree.set(&"parameters/Run/blend_position", go.x)
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-		# always false
+	
+	if velocity.y > 0:
+		state = State.FALL
+	elif velocity.y < 0:
+		state = State.JUMP
+	
+	if go and velocity.y == 0:
+		state = State.RUN
 
 	move_and_slide()
